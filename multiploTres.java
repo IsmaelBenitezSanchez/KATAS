16cc2a8dc882f05e5231b5e831736ea466dd3d5a
Ismael Benítez <ibenitezsanchez@cifpfbmoll.eu>
2025-11-11 11:53:06 +0100

    Pruebas

---
+++ multiploTres.java
@@ -0,0 +1,45 @@
+public class multiploTres {
+    /*
+    Crea una función en Java que reciba un array de números enteros. Por cada número, 
+    si el número es múltiplo de 3, guarda el número dividido entre 3 en esa posición de 
+    un nuevo array. Si el número no es múltiplo de 3, guarda el triple del número en esa
+    posición del nuevo array. Devuelve el nuevo array como resultado.
+    */
+    public static int[] multiploTres (int[] numero) {
+        int[] resultado = new int[numero.length];
+
+        for (int i = 0; i < numero.length; i++) {
+            if (numero[i] % 3 == 0) {
+                resultado[i] = numero[i] / 3;
+            } else {
+                resultado[i] = numero[i] * 3;
+            }
+        }
+        return resultado;
+    }
+}
+
+
+    /* Lo mismo pero con 5 */
+
+    int[] resultado = new int[numeros.length];
+
+    for (int i = 0; i < numeros.length; i++) {
+        if (numero[i] % 5) {
+            resultado[i] = numero[i] / 5;
+        } else {
+            resultado[i] = numero[i] * 3;
+        }
+    }
+    return resultado;
+
+
+    /* Lo mismo con 8 */
+    int[] resultado = new int[numeros.length];
+    for (int i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 8) {
+            resultado[i] = numeros[i] / 8;
+        }else {
+            resultado[i] = numeros[i] * 3;
+        }
+    }
