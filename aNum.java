6233e96537412aa0c32f7f245c1052a319b0eb16
Ismael Benítez <ibenitezsanchez@cifpfbmoll.eu>
2025-11-11 11:25:00 +0100

    ExamenMaximo

---
+++ aNum.java
@@ -0,0 +1,17 @@
+public class aNum {
+    // Devolver el numero entero representado por los 8 primeros digitos del NIF
+    public static int aDigito (char c) {
+        return c - '0';
+    }
+    public static long aNum(char[] a) {
+        // Usamos un acumulador de tipo long por seguridad, ya que 8 digitos
+        // pueden superar el limite de int. Extraemos cada digitp y construimos
+        // el numero como hariamos manualmente 
+        long valor = 0;
+        for (int i = 0; i < 8; i++) {
+            // Acumulacion decimal
+            valor = valor * 10 + aDigito(a[i]);
+        }
+        return valor
+    }
+}
