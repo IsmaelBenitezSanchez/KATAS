de97fc40135d9d594b4ab6c6aa0151d379ef72e0
DESKTOP-657SOLU\isric <ibenitezsanchez@cifpfbmoll.eu>
2025-11-12 11:24:43 +0100

    .

--- findMissingLetter.java
+++ findMissingLetter.java
@@ -5,8 +5,8 @@ public class findMissingLetter {
             // Calculamos diferencia entre el codigo del caracter actual y el siguiente
             // Si la diferencia no es 1, eso indica que entre ellos falta un caracter
             if (array[i + 1] - array[i] != 1) {
-                // Devolvemos el ccaracter faltante, sumando 1 al caracter actual
-                return (char)(current + 1);
+                // Devolvemos el caracter faltante, sumando 1 al caracter actual
+                return (char)(array[i] + 1);
             }
         }
         // Si no falta ninguna letra, devolvemos caracter nulo como valor por defecto
--- invertirNumero.java
+++ invertirNumero.java
@@ -11,8 +11,8 @@ public class invertirNumero {
     return true;
 }
     public static int invertirNumero (int numero) {
-        int numero = 0;
         int resto;
+        int numeroInvertido = 0;
         while (numero > 0) {
             resto = numero % 10;
             numeroInvertido = (numeroInvertido * 10) + resto;
