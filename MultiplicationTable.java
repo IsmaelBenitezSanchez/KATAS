e5dcda03b62b03e71f2b26626b38749b18f871f9
DESKTOP-657SOLU\isric <ibenitezsanchez@cifpfbmoll.eu>
2025-11-21 10:51:46 +0100

    .

---
+++ MultiplicationTable.java
@@ -0,0 +1,12 @@
+public class MultiplicationTable {
+    public static int[][] table (int [][] array) {
+        int n = array.length;
+        int[][] tabla = new int [n][n];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                tabla[i][j] = (i + 1)*(j + 1);
+            }
+        }
+        return tabla;
+    }
+}
