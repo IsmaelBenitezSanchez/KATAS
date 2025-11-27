b7c584f58cc64961ec5aa579a0813b659947b7f0
Ismael Benítez <ibenitezsanchez@cifpfbmoll.eu>
2025-11-27 11:36:26 +0100

    .

--- solved.java
+++ solved.java
@@ -16,7 +16,7 @@ public class solved {
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 if (board[i][j] == 0){
-                return -1;
+                    return -1;
                 }
             }
         }
