66cf1d4dbc29e44350463a21275317ada292792b
DESKTOP-657SOLU\isric <ibenitezsanchez@cifpfbmoll.eu>
2026-01-02 13:51:47 +0100

    .

--- EjExamen2.java
+++ EjExamen2.java
@@ -1,4 +1,4 @@
-public class kata {
+public class EjExamen2 {
     public static String[] losAlquilados(int[] recaudacion, String[] matriculas) {
         String[] resultado = new String[matriculas.length];
         for (int i = 0; i < matriculas.length; i++) {
